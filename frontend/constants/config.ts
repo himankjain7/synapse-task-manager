1b0b4b2450b8a09a6c89a74b1200d99b98477eea
Himank Jain <himankjain2007@gmail.com>
2026-06-28 18:54:07 +0530

    Update frontend API and Socket URL for production

--- frontend/constants/config.ts
+++ frontend/constants/config.ts
@@ -1,8 +1,12 @@
 export const Config = {
   API_URL:
-  process.env.EXPO_PUBLIC_API_URL ||
-  'https://synapse-backend-dolk.onrender.com',
-SOCKET_URL: process.env.EXPO_PUBLIC_SOCKET_URL || 'http://192.168.1.5:5000',
+    process.env.EXPO_PUBLIC_API_URL ||
+    'https://synapse-backend-dolk.onrender.com',
+
+  SOCKET_URL:
+    process.env.EXPO_PUBLIC_SOCKET_URL ||
+    'https://synapse-backend-dolk.onrender.com',
+
   API_TIMEOUT: 15000,
   IS_PRODUCTION: !__DEV__,
 } as const;
